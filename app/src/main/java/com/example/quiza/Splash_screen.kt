7c23444d57352c966e1caa573410c50e86b0ce13
Pavankumar75 <sripadapavan75@gmail.com>
2024-04-29 19:58:24 +0530

    FINAL COMMIT

---
+++ app/src/main/java/com/example/quiza/Splash_screen.kt
@@ -0,0 +1,19 @@
+package com.example.quiza
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close the splash screen activity
+        }, 3000)
+    }
+}
